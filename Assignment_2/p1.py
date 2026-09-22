02d53cfb99ef23b200e3d761bf29e4c65bef9f03
Aditya Jain <aj567053@gmail.com>
2026-09-22 08:55:12 +0530

    updated work

---
+++ Assignment_2/p1.py
@@ -0,0 +1,21 @@
+l=[]
+while True:
+    ch=int(input("Enter choice: \n 1. to add element qat end of list \n 2. to add at specific location \n 3 to delete specific element \n other no to exit program"))
+    if ch==1:
+        ele=input("Enter element you want to add: ")
+        l.append(ele)
+        print(l)
+    elif(ch==2):
+        ele=input("Enter element you want to add: ")
+        pos=int(input("Enter position you want to add element: "))
+        l.insert(pos,ele)
+        print(l)
+    elif(ch==3):
+        ele=input("Enter element you want to add: ")
+        l.remove(ele)
+        print(l)
+    else:
+        break
+        
+    
+    
